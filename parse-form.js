9ec531bd006b7efaa395d0f3cb6eab1d765adb80
Adam Brodzinski <adambrodzinski@gmail.com>
2014-01-12 19:46:16 -0800

    save input value and input jQ object to keys
    loop through inputs array and save. e.g.
    form.$name.val() >>> john doe
    form.name >>>           'john doe'

--- parse-form.js
+++ parse-form.js
@@ -15,5 +15,17 @@ ParseForm = function(elem) {
 
   // save each input into array, e.g. [{name: 'email', value:'jdoe@gmail.com'}]
   this.inputs = this.$el.serializeArray();
+
+  // itterate through all inputs and set their 'name' attr to the key & set the
+  // input value to the key's value. let's you call form.email to grab value.
+  // TODO refactor into for loop
+  $.each(this.inputs, function(i, el){
+    var name = self.inputs[i].name;
+    // save input value, e.g. form.$email >>> 'jdoe@gmail.com'
+    self[name] = self.inputs[i].value;
+    // save jquery cached element, e.g. form.$email
+    self['$' + name] = self.$el.find("input:nth-child("+ (i+1) +")");
+  });
+
 };
 
