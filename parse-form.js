d8dbf03084c9f263c538a5d0cc2f92793e7e4453
Adam Brodzinski <adambrodzinski@gmail.com>
2014-01-12 19:39:38 -0800

    cache form jQ object
    if the user provides a raw DOM node, cache it in this.el. Cache a jQuery
    object from either a string query or raw DOM node.

--- parse-form.js
+++ parse-form.js
@@ -4,6 +4,14 @@ ParseForm = function(elem) {
   var self = this;
 
   if (!elem) return console.error('You must provide an element or selector');
+  
+  // save dom node if it's not a jquery selector
+  if (typeof this.el !== 'string') {
+    this.el = elem;
+  }
+
+  // cache form jQuery object
+  this.$el = $(elem);
 
 };
 
